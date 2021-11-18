93dc0b321752cbec4371d5cf7bea7d78e579ddd4
Leandro O. Bastos <obastosleandro@gmail.com>
2021-11-18 17:41:00 -0300

    finaliza requisito 01

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
+  const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(URL);
+  const JSON = await response.json();
+  return JSON;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
+  const response = await fetch(URL);
+  const JSON = await response.json();
+  return JSON;
 }
