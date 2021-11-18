08f8874ab4964c4bb22371c0e97893ffe405957c
Adran <adran.carnavale@gmail.com>
2021-11-18 20:04:39 -0300

    iniciando requisitos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
+import InitialPage from './Components/InitialPage';
 import './App.css';
+
+// Mantendo o formato original do projeto
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" render={ () => <InitialPage /> } />
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/InitialPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class InitialPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Eu sou o componente InitialPage</h1>
+      </div>
+    );
+  }
+}
+
+export default InitialPage;
