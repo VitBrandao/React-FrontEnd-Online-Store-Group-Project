feb605f1d707b4bfc8b21e11a52d09c1c21dad41
vitor <vitorsbsilva@gmail.com>
2021-11-19 15:29:09 -0300

    Req.3 - feito

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import InitialPage from './Components/InitialPage';
+import ShoppingCart from './pages/ShoppingCart';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" render={ () => <InitialPage /> } />
+      <Route exact path="/shoppingcart" render={ () => <ShoppingCart /> } />
+      <Link to="/shoppingcart" data-testid="shopping-cart-button">
+        <button> Enviar </button>
+      </Link>
     </BrowserRouter>
   );
 }
